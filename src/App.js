ebb658dd565806031bc83937499d0be60b0309bf
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2024-09-06 17:20:56 +0200

    articleList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+function ArticleList() {
+  const articles = [
+    { title: 'React Basics', description: 'Introduction to React.'},
+    { title: 'State and Props', description: 'How State and Props works.'},
+    { title: 'useEffect Hook', description: 'SideEffect gestion.'}
+  ];
+
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredArticles, setFilteredArticles] = useState(articles);
+
+  useEffect(() => {
+    const results = articles.filter(article => article.title.toLowerCase().includes(searchInput.toLowerCase()));
+    setFilteredArticles(results);
+  }, [searchInput]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input placeholder='Cerca Articoli' onChange={(e) => setSearchInput(e.target.value)}></input>
+      </div>
+
+      <div>
+        <ul>
+          {filteredArticles.map((articles,index) => (
+            <li key={index}>
+              <h2>{articles.title}</h2>
+              <p>{articles.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default ArticleList;
